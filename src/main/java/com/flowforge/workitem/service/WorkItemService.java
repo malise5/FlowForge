5983ad87ff4d9eb2bf93a5ce567f81637c03cfb8
Halkano Malise <kudezx@gmail.com>
2026-01-31 17:18:33 +0300

    Refactor WorkItemService to improve performance

--- src/main/java/com/flowforge/workitem/service/WorkItemService.java
+++ src/main/java/com/flowforge/workitem/service/WorkItemService.java
@@ -1,8 +1,11 @@
 package com.flowforge.workitem.service;
 
 import com.flowforge.workitem.domain.entity.WorkItem;
+import com.flowforge.workitem.dto.CreateWorkItemRequest;
 import com.flowforge.workitem.dto.WorkItemResponse;
+import java.util.UUID;
 
 public interface WorkItemService {
-    WorkItemResponse create(String title, String description);
+    WorkItemResponse create(CreateWorkItemRequest req);
+    void delete(UUID id);
 }
