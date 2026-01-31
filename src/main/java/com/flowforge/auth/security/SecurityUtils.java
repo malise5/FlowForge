bebdde1e26c732d525079930db2337858ee9c38c
Halkano Malise <kudezx@gmail.com>
2026-01-31 17:17:59 +0300

    Add utility methods in SecurityUtils for token validation

--- src/main/java/com/flowforge/auth/security/SecurityUtils.java
+++ src/main/java/com/flowforge/auth/security/SecurityUtils.java
@@ -1,4 +1,19 @@
 package com.flowforge.auth.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class SecurityUtils {
+
+    public static String getCurrentUserEmail() {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        assert auth != null;
+        return auth.getName();
+    }
+
+    public static boolean hasRole(String role) {
+        return SecurityContextHolder.getContext().getAuthentication()
+                .getAuthorities()
+                .stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_" + role));
+    }
 }
