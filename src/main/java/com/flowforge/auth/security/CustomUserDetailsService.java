fcdb9e566dbd438907c5414103855d0ab950426a
Halkano Malise <kudezx@gmail.com>
2026-01-31 17:17:45 +0300

    Enhance CustomUserDetailsService with custom user loading logic

--- src/main/java/com/flowforge/auth/security/CustomUserDetailsService.java
+++ src/main/java/com/flowforge/auth/security/CustomUserDetailsService.java
@@ -1,4 +1,28 @@
 package com.flowforge.auth.security;
 
-public class CustomUserDetailsService {
+import com.flowforge.auth.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+
+        return User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
 }
