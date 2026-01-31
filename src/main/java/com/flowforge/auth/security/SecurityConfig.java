df3c0e8fa1d2268e513d0e736783e592a0c510dc
Halkano Malise <kudezx@gmail.com>
2026-01-31 17:17:51 +0300

    Refactor SecurityConfig for better security settings

--- src/main/java/com/flowforge/auth/security/SecurityConfig.java
+++ src/main/java/com/flowforge/auth/security/SecurityConfig.java
@@ -1,13 +1,18 @@
-package com.flowforge.workitem.security;
+package com.flowforge.auth.security;
 
+import com.flowforge.auth.dto.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
